9bc0bb0868f1faa74af3277705f0abd3fc7b169a
doginnn <diogenesemmanuel@gmail.com>
2020-03-16 16:56:39 -0300

    [UPD]Atualizando a app SGD_Heros

--- dev/cadastro/views.py
+++ dev/cadastro/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def login(request):
+    return render(request, 'login.html')
+
+def home(request):
+    return render(request, 'home.html')
+
+def base(request):
+    return render(request, 'base.html')
--- dev/sgd_heros/settings.py
+++ dev/sgd_heros/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
 #  APP Personalizados
-    'login',
+    'cadastro',
 
 ]
 
