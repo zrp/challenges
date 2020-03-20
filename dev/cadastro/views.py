d41bc8da717469bb16cb4dd7ce8f73f26a59f5bc
doginnn <diogenesemmanuel@gmail.com>
2020-03-20 14:48:21 -0300

    [UPD]Refatorando views.py

--- dev/cadastro/views.py
+++ dev/cadastro/views.py
@@ -19,14 +19,15 @@ def cadastrar_hero(request):
             return redirect('logar_hero')
     else:
         form_cadastro = UserCreationForm()
-    return render(request, 'cadastrar.html', {"form_cadastro":form_cadastro})
+    return render(request, 'cadastrar.html', {"form_cadastro": form_cadastro})
 
 
 def logar_hero(request):
     if request.method == 'POST':
         username = request.POST['username']
         password = request.POST['password']
-        form_login = authenticate(request, username=username, password=password)
+        form_login = authenticate(
+            request, username=username, password=password)
         if form_login is not None:
             login(request, form_login)
             return redirect('home')
@@ -36,32 +37,33 @@ def logar_hero(request):
             return redirect('logar_hero')
     else:
         form_login = AuthenticationForm()
-    return render(request, 'login.html', {"form_login":form_login})
+    return render(request, 'login.html', {"form_login": form_login})
 
 
 def atualizar_senha(request):
     if request.method == "POST":
         form_senha = PasswordChangeForm(request.user, request.POST)
         if form_senha.is_valid():
-            user = form_senha.save()            
+            user = form_senha.save()
             update_session_auth_hash(request, user)
             return redirect('home')
-            messages.success(request, 'Senha atualizada com sucesso!')            
+            messages.success(request, 'Senha atualizada com sucesso!')
         else:
             messages.error(request, 'Desculpe, isso não funcionou!')
             messages.error(request, 'Por gentileza tente novamente.')
             return redirect('atualizar_senha')
     else:
         form_senha = PasswordChangeForm(request.user)
-    return render(request, 'reset_senha.html', {"form_senha":form_senha})
+    return render(request, 'reset_senha.html', {"form_senha": form_senha})
 
 
 def deslogar_hero(request):
     logout(request)
     return redirect('home')
 
-
     # Views referente ao Herói
+
+
 def cria_hero(request):
     if request.method == 'POST':
         form_cria_hero = UserCreationForm(request.POST)
@@ -70,7 +72,7 @@ def cria_hero(request):
             return redirect('logar_hero')
     else:
         form_cria_hero = UserCreationForm()
-    return render(request, 'cria_hero.html', {"form_cria_hero":form_cria_hero})
+    return render(request, 'cria_hero.html', {"form_cria_hero": form_cria_hero})
 
 
 def edita_hero(request):
