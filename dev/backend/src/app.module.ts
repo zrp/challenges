849ba10781d384994545a903ffe3bb82a691e079
Celso Chagas <jjuniorchagas15@gmail.com>
2023-05-19 17:50:44 -0300

    feat: configure providers in app module

--- dev/backend/src/app.module.ts
+++ dev/backend/src/app.module.ts
@@ -6,12 +6,34 @@ import { AuthModule } from './auth/auth.module';
 import { JwtAuthGuard } from './auth/guards/jwt-auth.guard';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
+import { HerosModule } from './heros/heros.module';
+import { ThreatsModule } from './threats/threats.module';
+import { ThreatsGateway } from './threats/threats.gateway';
+import { ThreatsService } from './threats/threats.service';
+import { HistoryModule } from './history/history.module';
+import { HistoryController } from './history/history.controller';
+import { UserController } from './user/user.controller';
+import { HerosController } from './heros/heros.controller';
 
 @Module({
-  imports: [PrismaModule, UserModule, AuthModule],
-  controllers: [AppController],
+  imports: [
+    PrismaModule,
+    UserModule,
+    AuthModule,
+    HerosModule,
+    ThreatsModule,
+    HistoryModule,
+  ],
+  controllers: [
+    AppController,
+    HistoryController,
+    UserController,
+    HerosController,
+  ],
   providers: [
     AppService,
+    ThreatsGateway,
+    ThreatsService,
     {
       provide: APP_GUARD,
       useClass: JwtAuthGuard,
